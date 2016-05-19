e3d75d484870c060d410d2c54c9b2a5edf48fb17
Johan Jäderblom <johja322@student.liu.se>
2016-05-19 18:39:16 +0200

    receives the hhtp request now

---
+++ GameJS/game.js
@@ -0,0 +1,8 @@
+var rounds = [];
+
+var over = false;
+var win = false;
+
+function updateRound(roundnr, round){
+  rounds[roundnr] = round;
+}
---
+++ pages/livestats/jsonhandler.js
@@ -0,0 +1,52 @@
+var jobject;
+var statskills = document.getElementById("statskills");
+var statsdeaths = document.getElementById("statsdeaths");
+var statsassists = document.getElementById("statsassists");
+var statsmvp = document.getElementById("statsmvps");
+var statskd = document.getElementById("statsKDs");
+var statshs = document.getElementById("statsHSs");
+var statsscore =document.getElementById("statsscore");
+
+statskd.innerHTML = 0;
+statshs.innerHTML = 0;
+
+
+ipcRenderer.on('json', (event, message) => {
+      console.log(message);
+      jobject = JSON.parse(message);
+      if(jobject.provider.steamid == jobject.player.steamid)
+        populatelive();
+
+      if(jobject.map != null)
+        updatescorechart();
+    });
+
+    function updatescorechart(){
+      playerteam = jobject.player.team;
+      var win = 0;
+      var loss = 0;
+      if(playerteam == "CT"){
+        win = jobject.map.team_ct.score;
+        loss = jobject.map.team_t.score;
+      }
+      else {
+        win =  jobject.map.team_t.score;
+        loss = jobject.map.team_ct.score;
+      }
+
+      var str = '{"win":"'+win+'", "loss":"'+loss+'" }';
+      ipcRenderer.send('scorechart', str);
+
+    }
+
+
+    function populatelive(){
+      var matchstats = jobject.player.match_stats;
+      if(matchstats != null){
+        statskills.innerHTML = matchstats.kills;
+        statsassists.innerHTML = matchstats.assists;
+        statsdeaths.innerHTML = matchstats.deaths;
+        statsmvp.innerHTML = matchstats.mvps;
+        statsscore.innerHTML = matchstats.score;
+      }
+    }
